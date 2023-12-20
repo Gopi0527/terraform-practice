d337aed693af692b107af6e1585e5b3b849302ff
Gopi0527 <gopikrishna0527@gmail.com>
2023-12-20 17:10:04 +0530

    added subnets

--- Aws/main.tf
+++ Aws/main.tf
@@ -6,3 +6,30 @@ resource "aws_vpc" "first" {
   }
 
 }
+resource "aws_subnet" "web" {
+  vpc_id     = aws_vpc.first.id
+  cidr_block = "10.10.0.0/24"
+  tags = {
+    Name = "web"
+  }
+  depends_on = [aws_vpc.first]
+
+}
+resource "aws_subnet" "business" {
+  vpc_id     = aws_vpc.first.id
+  cidr_block = "10.10.1.0/24"
+  tags = {
+    Name = "business"
+  }
+  depends_on = [aws_vpc.first]
+
+}
+resource "aws_subnet" "data" {
+  vpc_id     = aws_vpc.first.id
+  cidr_block = "10.10.2.0/24"
+  tags = {
+    Name = "data"
+  }
+  depends_on = [aws_vpc.first]
+
+}
